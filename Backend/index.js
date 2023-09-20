acce0636dc532a2b0b6b00747b7c33a04c369231
bibek142 <140243205+bibek142@users.noreply.github.com>
2023-09-21 02:09:06 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ const app = express()
 const port = 5000
 app.use(cors(
   {
-    origin: ["https://deploy-mern-1whq.vercel.app"],
+    origin: [""],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   }
