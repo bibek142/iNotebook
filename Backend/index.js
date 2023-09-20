93284f2ee2e285f925f32b096a8d8974104a2aa7
bibek142 <140243205+bibek142@users.noreply.github.com>
2023-09-21 01:55:45 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -16,6 +16,9 @@ app.use(cors(
 
 app.use(express.json());
 
+app.get("/", (req, res)=>{
+  res.json("Hello");
+})
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
